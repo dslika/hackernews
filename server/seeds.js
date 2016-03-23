1b28fbdccf3620bd6d608dc648937981facbd6e5
k_ri <ri@kizuna.co.jp>
2016-03-23 18:43:30 +0900

    add User login.

--- server/seeds.js
+++ server/seeds.js
@@ -1,14 +1,22 @@
 if (Posts.find().count() === 0) {
   Posts.insert({
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
+    title: 'KizunaJapan',
+    url: 'http://www.kizuna.co.jp'
   });
   Posts.insert({
-    title: 'Meteor',
-    url: 'http://meteor.com'
+    title: 'Doreming(USA)',
+    url: 'http://www.doreming.com'
   });
   Posts.insert({
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
+    title: 'Doreming(JPN)',
+    url: 'http://www.doreming.com/ja'
+  });
+  Posts.insert({
+    title: '吉野家',
+    url: 'http://www.yoshinoya.com/menu/index.html'
+  });
+  Posts.insert({
+    title: 'CoCo壱番屋',
+    url: 'http://www.ichibanya.co.jp/menu/index.html'
   });
 }
